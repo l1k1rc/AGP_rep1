dd2eec53391054b04f01d411fb466b548f8527e4
l1k1rc <throwaraccoon@gmail.com>
2020-01-14 17:16:37 +0100

    Add mix request 1.1

--- src/persistence/jdbc/JdbcPersistence.java
+++ src/persistence/jdbc/JdbcPersistence.java
@@ -7,91 +7,84 @@ import java.sql.SQLException;
 import dao.StatisticPersistence;
 
 public class JdbcPersistence implements StatisticPersistence {
-	
+
 	@Override
 	public void dataInit() {
-		System.err.println("Please don't forget to create tables manually by importing creation.sql in your database !");
+		System.err
+				.println("Please don't forget to create tables manually by importing creation.sql in your database !");
 	}
 
-	/*@Override
-	public int persist(SimulationEntry simulationEntry, StatisticManager statisticManager) {
-		int idEntry = persistEntry(simulationEntry);
-		if (idEntry != 0) {
-			persistClients(statisticManager, idEntry);
-		}
-		return idEntry;
-
-	}
-
-	private void persistClients(StatisticManager statisticManager, int idEntry) {
-		List<AbstractClient> servedClients = statisticManager.getServedClients();
-
-		for (AbstractClient client : servedClients) {
-			addClient(idEntry, client, true);
-		}
-
-		List<AbstractClient> nonServedClients = statisticManager.getNonServedClients();
-
-		for (AbstractClient client : nonServedClients) {
-			addClient(idEntry, client, false);
-		}
-
-	}
-
-	private void addClient(int idEntry, AbstractClient client, boolean isServed) {
-		try {
-
-			String insertAddressQuery = "INSERT INTO client (arrival_time, service_start_time, departure_time, "
-					+ "is_served, priority, entry_id) VALUES (?,?,?,?,?,?)";
-
-			PreparedStatement preparedStatement = JdbcConnection.getConnection().prepareStatement(insertAddressQuery);
-
-			preparedStatement.setInt(1, client.getArrivalTime());
-			preparedStatement.setInt(2, client.getServiceStartTime());
-			preparedStatement.setInt(3, client.getDepartureTime());
-			preparedStatement.setBoolean(4, isServed);
-			preparedStatement.setBoolean(5, client.isPriority());
-			preparedStatement.setInt(6, idEntry);
-
-			preparedStatement.executeUpdate();
-
-			preparedStatement.close();
-		} catch (SQLException se) {
-			System.err.println(se.getMessage());
-		}
-	}
-
-	private int persistEntry(SimulationEntry simulationEntry) {
-		int idEntry = 0;
-		try {
-
-			String insertAddressQuery = "INSERT INTO entry (simulation_duration, cashier_count, "
-					+ "min_service_time, max_service_time, client_arrival_interval, priority_client_rate, client_patience_time"
-					+ ") VALUES (?,?,?,?,?,?,?)";
-
-			PreparedStatement preparedStatement = JdbcConnection.getConnection().prepareStatement(insertAddressQuery);
-
-			// Set values of parameters in the query.
-			preparedStatement.setInt(1, simulationEntry.getSimulationDuration());
-			preparedStatement.setInt(2, simulationEntry.getCashierCount());
-			preparedStatement.setInt(3, simulationEntry.getMinServiceTime());
-			preparedStatement.setInt(4, simulationEntry.getMaxServiceTime());
-			preparedStatement.setInt(5, simulationEntry.getClientArrivalInterval());
-			preparedStatement.setDouble(6, simulationEntry.getPriorityClientRate());
-			preparedStatement.setInt(7, simulationEntry.getClientPatienceTime());
-
-			preparedStatement.executeUpdate();
-
-			ResultSet keys = preparedStatement.getGeneratedKeys();
-			keys.next();
-			idEntry = keys.getInt(1);
-
-			preparedStatement.close();
-		} catch (SQLException se) {
-			System.err.println(se.getMessage());
-		}
-		return idEntry;
-	}*/
+	/*
+	 * @Override public int persist(SimulationEntry simulationEntry,
+	 * StatisticManager statisticManager) { int idEntry =
+	 * persistEntry(simulationEntry); if (idEntry != 0) {
+	 * persistClients(statisticManager, idEntry); } return idEntry;
+	 * 
+	 * }
+	 * 
+	 * private void persistClients(StatisticManager statisticManager, int idEntry) {
+	 * List<AbstractClient> servedClients = statisticManager.getServedClients();
+	 * 
+	 * for (AbstractClient client : servedClients) { addClient(idEntry, client,
+	 * true); }
+	 * 
+	 * List<AbstractClient> nonServedClients =
+	 * statisticManager.getNonServedClients();
+	 * 
+	 * for (AbstractClient client : nonServedClients) { addClient(idEntry, client,
+	 * false); }
+	 * 
+	 * }
+	 * 
+	 * private void addClient(int idEntry, AbstractClient client, boolean isServed)
+	 * { try {
+	 * 
+	 * String insertAddressQuery =
+	 * "INSERT INTO client (arrival_time, service_start_time, departure_time, " +
+	 * "is_served, priority, entry_id) VALUES (?,?,?,?,?,?)";
+	 * 
+	 * PreparedStatement preparedStatement =
+	 * JdbcConnection.getConnection().prepareStatement(insertAddressQuery);
+	 * 
+	 * preparedStatement.setInt(1, client.getArrivalTime());
+	 * preparedStatement.setInt(2, client.getServiceStartTime());
+	 * preparedStatement.setInt(3, client.getDepartureTime());
+	 * preparedStatement.setBoolean(4, isServed); preparedStatement.setBoolean(5,
+	 * client.isPriority()); preparedStatement.setInt(6, idEntry);
+	 * 
+	 * preparedStatement.executeUpdate();
+	 * 
+	 * preparedStatement.close(); } catch (SQLException se) {
+	 * System.err.println(se.getMessage()); } }
+	 * 
+	 * private int persistEntry(SimulationEntry simulationEntry) { int idEntry = 0;
+	 * try {
+	 * 
+	 * String insertAddressQuery =
+	 * "INSERT INTO entry (simulation_duration, cashier_count, " +
+	 * "min_service_time, max_service_time, client_arrival_interval, priority_client_rate, client_patience_time"
+	 * + ") VALUES (?,?,?,?,?,?,?)";
+	 * 
+	 * PreparedStatement preparedStatement =
+	 * JdbcConnection.getConnection().prepareStatement(insertAddressQuery);
+	 * 
+	 * // Set values of parameters in the query. preparedStatement.setInt(1,
+	 * simulationEntry.getSimulationDuration()); preparedStatement.setInt(2,
+	 * simulationEntry.getCashierCount()); preparedStatement.setInt(3,
+	 * simulationEntry.getMinServiceTime()); preparedStatement.setInt(4,
+	 * simulationEntry.getMaxServiceTime()); preparedStatement.setInt(5,
+	 * simulationEntry.getClientArrivalInterval()); preparedStatement.setDouble(6,
+	 * simulationEntry.getPriorityClientRate()); preparedStatement.setInt(7,
+	 * simulationEntry.getClientPatienceTime());
+	 * 
+	 * preparedStatement.executeUpdate();
+	 * 
+	 * ResultSet keys = preparedStatement.getGeneratedKeys(); keys.next(); idEntry =
+	 * keys.getInt(1);
+	 * 
+	 * preparedStatement.close(); } catch (SQLException se) {
+	 * System.err.println(se.getMessage()); } return idEntry; }
+	 */
 
 	@Override
 	public int servedClientCount(int simulationEntryId) {
@@ -137,13 +130,8 @@ public class JdbcPersistence implements StatisticPersistence {
 		} catch (SQLException se) {
 			System.err.println(se.getMessage());
 		}
-		
+
 		return count;
 	}
-
-
-
-
-
 
 }
--- src/persistence/jdbc/Mixrequest.java
+++ src/persistence/jdbc/Mixrequest.java
@@ -1,29 +1,66 @@
 package persistence.jdbc;
 
+import java.util.ArrayList;
+
 import lucene.LuceneTester;
 
 public class Mixrequest {
 	private String request;
-	
+	private ArrayList<String> fileKeyIndex = new ArrayList<String>();
+
+	public Mixrequest() {
+	}
 	public static void main(String[] args) {
-		Mixrequest mixrequest = new Mixrequest("SELECT nom, clé FROM T with sculpture voyage;");
-		String[] s=mixrequest.getSeparatedValue();
-		System.out.println(s[0]+" :::: "+s[1]);
+		Mixrequest mixrequest = new Mixrequest();
+		mixrequest.getIndexFileNumberForMixRequest();
+	}
+	public void getIndexFileNumberForMixRequest() {
+		Mixrequest mixrequest = new Mixrequest("SELECT nom, clé FROM T with sculpture fuerteventura;");
+		String[] s = mixrequest.getSeparatedValue();
+		System.out.println(s[0] + " :::: " + s[1]);
 		LuceneTester.searchResult(s[1]);
 		System.out.println(LuceneTester.getIndexFile());
+		fileKeyIndex = mixrequest.getIndexNumberOf(LuceneTester.getIndexFile());
+		System.out.println(fileKeyIndex);
 	}
+
+	/**
+	 * This method gotta split the path received to retrieve only the name of the
+	 * file.
+	 * 
+	 * @param data : is the path of the indexed file
+	 * @return
+	 */
+	public ArrayList<String> getIndexNumberOf(ArrayList<String> data) {
+		String separator = "/";
+		ArrayList<String> resultS = new ArrayList<String>();
+		for (String s : data) {
+			System.out.println(s);
+			String[] segmentation = s.split(separator);
+			resultS.add(segmentation[segmentation.length - 1].replaceAll(".[^.]*\\z", ""));
+		}
+		return resultS;
+	}
+
+	public ArrayList<String> getFileKeyIndex() {
+		return fileKeyIndex;
+	}
+
 	public Mixrequest(String request) {
 		this.request = request;
 	}
+
 	public String[] getSeparatedValue() {
-		String separator ="with";
+		String separator = "with";
 		return request.split(separator);
 	}
+
 	public String getRequest() {
 		return request;
 	}
+
 	public void setRequest(String request) {
 		this.request = request;
 	}
-	
+
 }
