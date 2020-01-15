0b15f65b88f06da9f49491d6231e5d27b1b1e2ee
Fatma Abdelhamid <fatma.abdelhamid2016@yahoo.fr>
2020-01-15 11:26:03 +0100

    test api

--- src/persistence/API/testAPI.java
+++ src/persistence/API/testAPI.java
@@ -3,6 +3,12 @@ package persistence.API;
 public class testAPI {
 	public static void main(String[] args) {
 		APIDatabase apiMix = new MixOperator();
-		System.out.println("Résultat final : " + apiMix.queryAPI("SELECT * FROM site WHERE price=50 with plongee"));
+		System.out.println("Résultat final : " + apiMix.queryAPI("SELECT * FROM site WHERE price=50 with plongée"));
+		
+		APIDatabase apiSql = new SQLOperator();
+		System.out.println("Résultat final : " + apiSql.queryAPI("SELECT * FROM site WHERE price=50"));
+
+		
+
 	}
 }
