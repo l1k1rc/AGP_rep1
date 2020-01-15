e1c0e6bdc811b960a6ba9aa527be1a7641397305
l1k1rc <throwaraccoon@gmail.com>
2020-01-15 11:31:06 +0100

    Update API +++

--- src/persistence/API/testAPI.java
+++ src/persistence/API/testAPI.java
@@ -3,12 +3,13 @@ package persistence.API;
 public class testAPI {
 	public static void main(String[] args) {
 		APIDatabase apiMix = new MixOperator();
-		System.out.println("Résultat final : " + apiMix.queryAPI("SELECT * FROM site WHERE price=50 with plongée"));
-		
-		APIDatabase apiSql = new SQLOperator();
-		System.out.println("Résultat final : " + apiSql.queryAPI("SELECT * FROM site WHERE price=50"));
+		System.out.println("Résultat MIX : " + apiMix.queryAPI("SELECT * FROM site WHERE price=50 with plongée"));
 
-		
+		APIDatabase apiSql = new SQLOperator();
+		System.out.println("Résultat SQL : " + apiSql.queryAPI("SELECT * FROM site WHERE price=50"));
+
+		APIDatabase apiText = new TextualOperator();
+		System.out.println("Résultat Textuel : " + apiText.queryAPI("plongée"));
 
 	}
 }
